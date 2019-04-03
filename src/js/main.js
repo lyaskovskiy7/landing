ac5390140c0e0f75ff1e9b9e538737ef5f94f963
lyaskovskiy7 <lyaskovskiy7@gmai.com>
2019-04-03 23:16:29 +0300

    test task

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
-    $('.menu-adaptive').click(function(){
-        $('.nav').toggleClass('block')
+$(document).ready(function () {
+    $('.menu__link-active').click(function () {
+        $('.menu-dabl').toggleClass('display-block')
+    })
+
+    
+    var i = 0
+    var img = $('.sl-slide')
+    $('.next').click(function () {
+        img.eq(i).css('display', 'none')
+        i++
+        if (i >= img.length) {
+            i = 0
+        }
+        img.eq(i).css('display', 'block')
+    })
+    $('.prev').click(function () {
+        img.eq(i).css('display', 'none')
+        i--
+        if (i < 0) {
+            i = img.length - 1
+        }
+        img.eq(i).css('display', 'block')
     })
 
 })
